24a601aa17f9f62731f5e5b8f7d283d118920786
Kamil Ochnik <ilmak@o2.pl>
2020-07-13 19:21:08 +0200

    hmmm działa ... v1

--- src/EncodeAllData.java
+++ src/EncodeAllData.java
@@ -4,6 +4,17 @@ import java.security.*;
 import java.util.Scanner;
 import javax.crypto.*;
 
+/*
+program który pobiera plik.txt
+dodaje go do synchronizowanej listy (własna implementacja synchronizowanej listy)
+następnie uruchamiam 3 wątki szyfrujące które pobierają z listy dane,
+szyfrują je i zapisują w tym samym miejscu na lisice gdzie były dane nie zaszyfrowane.
+wątki sprawdzają czy na liście znajdują się jeszcze jakieś dane nie zaszyfrowane jeśli tak
+to pobierają pierwsze jakie znajdą do szyfrowania a jeśli wszystkie dane zostały zaszyfrowane kończy prace.
+
+Wynik pracy programu można ogladać w konsoli.
+ */
+
 public class EncodeAllData {
 
     private static MySynchronizedList list = new MySynchronizedList();
@@ -19,39 +30,42 @@ public class EncodeAllData {
             out.writeObject(key);
         }
 
-            try (Scanner reading = new Scanner(inputFile)) {
-                while (reading.hasNext()) {
-                    String line = reading.nextLine();
-                    System.out.println(line);
-                    list.addDataToList(line);
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+        try (Scanner reading = new Scanner(inputFile)) {
+            while (reading.hasNext()) {
+                String line = reading.nextLine();
+                System.out.println(line);
+                list.addDataToList(line);
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        while (!list.checkIfAllDataAreEncoded()) {
-            Runnable encodeDataInMyList = () ->{
+        for (int i = 1; i <= 3; i++) {
+            Runnable encodeDataInMyList = () -> {
 
-                DataAndIndex dataAndIndex = list.takeFirstDecoded();
-                ByteArrayInputStream in = new ByteArrayInputStream(dataAndIndex.getData().getBytes());
-                ByteArrayOutputStream out = new ByteArrayOutputStream(dataAndIndex.getData().getBytes().length);
-                try {
-                    encrypt(in, out);
-                } catch (IOException | GeneralSecurityException | ClassNotFoundException e) {
-                    e.printStackTrace();
+                while (!list.checkIfAllDataAreEncoded()) {
+
+                    DataAndIndex dataAndIndex = list.takeFirstDecoded();
+                    ByteArrayInputStream in = new ByteArrayInputStream(dataAndIndex.getData().getBytes());
+                    ByteArrayOutputStream out = new ByteArrayOutputStream(dataAndIndex.getData().getBytes().length);
+                    try {
+                        encrypt(in, out);
+                    } catch (IOException | GeneralSecurityException | ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                    list.saveEncodedData(new DataAndIndex(out.toString(), dataAndIndex.getArrayIndex()));
+                    System.out.println(" ");
+                    list.print();
                 }
-
-                list.saveEncodedData(new DataAndIndex(out.toString(), dataAndIndex.getArrayIndex()));
-                System.out.println(" ");
-                list.print();
             };
-            // 1 wątki
             new Thread(encodeDataInMyList).start();
         }
 
     }
 
-    private static void encrypt(InputStream in, ByteArrayOutputStream out) throws IOException, ClassNotFoundException, GeneralSecurityException {
+    private static void encrypt(InputStream in, ByteArrayOutputStream out) throws
+            IOException, ClassNotFoundException, GeneralSecurityException {
         int mode = Cipher.ENCRYPT_MODE;
 
         try (ObjectInputStream keyIn = new ObjectInputStream(new FileInputStream("key.txt"));) {
